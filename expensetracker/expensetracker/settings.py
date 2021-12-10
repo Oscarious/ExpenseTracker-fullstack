51f67b116df769d36601e7b94b2804b2b7752f95
Oscarious <oscarzqy@hotmail.com>
2021-12-10 11:58:23 -0330

    django secret_key

--- expensetracker/expensetracker/settings.py
+++ expensetracker/expensetracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .env.env import DJANGO_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u84%wu6ug$91wamltsm+#_ez1nxrbczxu@dp%d#3h4)8*wrh3('
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
