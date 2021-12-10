1da2427466a761ce3ac541afc872af9feb3e7625
Oscarious <oscarzqy@hotmail.com>
2021-12-10 10:42:04 -0330

    clear input after submit

--- expensetracker/frontend/src/components/AddTransaction.js
+++ expensetracker/frontend/src/components/AddTransaction.js
@@ -9,14 +9,17 @@ export const AddTransaction = (props) => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
-  const onClick = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
     if (text.length === 0) {
       store.dispatch(
-        errorMessage(EMPTY_TEXT_ERROR, {client: ["Text should not be empty"]})
+        errorMessage(EMPTY_TEXT_ERROR, { client: ["Text should not be empty"] })
       );
       return;
     }
     props.addTransaction({ text: text, amount: Number(amount) });
+    setText("");
+    setAmount(0);
   };
 
   return (
@@ -25,28 +28,32 @@ export const AddTransaction = (props) => {
         Add New Transaction
       </h2>
       <h3 className='mt-2'>Text</h3>
-      <input
-        type='text'
-        className='mt-1 border-2 w-full h-10'
-        placeholder='Enter Text...'
-        onChange={(e) => setText(e.target.value)}
-      />
-      <h3 className='mt-2'>Amount</h3>
-      <label className='tracking-tighter'>
-        (negative - expense, positive - income)
-      </label>
-      <input
-        type='number'
-        className='mt-1 border-2 w-full h-10'
-        placeholder='Enter Amount...'
-        onChange={(e) => setAmount(e.target.value)}
-      />
-      <button
-        className='mt-6 w-full h-12 rounded-md bg-indigo-400 transform hover:bg-indigo-300 active:-translate-y-1 transition active:bg-indigo-500'
-        onClick={onClick}
-      >
-        Add Transaction
-      </button>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          className='mt-1 border-2 w-full h-10'
+          placeholder='Enter Text...'
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <h3 className='mt-2'>Amount</h3>
+        <label className='tracking-tighter'>
+          (negative - expense, positive - income)
+        </label>
+        <input
+          type='number'
+          className='mt-1 border-2 w-full h-10'
+          placeholder='Enter Amount...'
+          onChange={(e) => setAmount(e.target.value)}
+          value={amount}
+        />
+        <button
+          type='submit'
+          className='mt-6 w-full h-12 rounded-md bg-indigo-400 transform hover:bg-indigo-300 active:-translate-y-1 transition active:bg-indigo-500'
+        >
+          Add Transaction
+        </button>
+      </form>
     </div>
   );
 };
